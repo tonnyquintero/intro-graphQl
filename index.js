9b5cab994644164eafce8f3f4e2f531fe79597a1
tonny187 <tonyoanthony@gmail.com>
2022-09-18 16:40:22 -0500

    finishing course

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require('dotenv').config()
 
 const { makeExecutableSchema } = require('graphql-tools');
 const express = require('express')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql');
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
 const app = express();
 const port = process.env.PORT 
+//const isDev = process.env.NODE_ENV !== 'production'
 
 const typeDefs = readFileSync (
     join(__dirname, 'lib', 'schema.graphql'),
@@ -30,6 +32,8 @@ const schema = makeExecutableSchema({
 // }).catch(e => {
 //     console.log(e)
 // })
+
+app.use(cors())
 
 app.use('/api', graphqlHTTP({
     schema: schema,
